81a6cae6b88374fe9782a379190bd28ef41f4cb9
Bishal <bishalbera2@gmail.com>
2023-06-03 15:24:24 +0530

    email input and text input field added

--- app/src/main/java/com/bishal/lazyreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/bishal/lazyreader/screens/login/ReaderLoginScreen.kt
@@ -1,9 +1,62 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.bishal.lazyreader.screens.login
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.navigation.NavHostController
+import com.bishal.lazyreader.screens.lottie.ReaderLogo
 
 @Composable
 fun ReaderLoginScreen(navController: NavHostController) {
 
+    val showLoginForm = rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            ReaderLogo()
+            if (showLoginForm.value) UserForm(loading = false, isCreateAccount = false){ email, password ->
+
+
+            }else{
+                UserForm(loading = false, isCreateAccount = true){ email, password ->
+
+
+                }
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = {email, pwd ->}
+) {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid  = remember(email.value, password.value){
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+
+    }
 }
