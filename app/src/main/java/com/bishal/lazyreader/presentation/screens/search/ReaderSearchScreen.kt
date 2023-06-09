1e1148b9e244d8976be45939581c66bea8a1dbc8
Bishal <bishalbera2@gmail.com>
2023-06-09 15:01:30 +0530

    books can be shown in search screen

--- app/src/main/java/com/bishal/lazyreader/presentation/screens/search/ReaderSearchScreen.kt
+++ app/src/main/java/com/bishal/lazyreader/presentation/screens/search/ReaderSearchScreen.kt
@@ -3,30 +3,52 @@
 package com.bishal.lazyreader.presentation.screens.search
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.bishal.lazyreader.domain.model.Item
 import com.bishal.lazyreader.navigation.BottomBar
+import com.bishal.lazyreader.navigation.ReaderScreen
 import com.bishal.lazyreader.presentation.common.InputField
 import com.bishal.lazyreader.presentation.common.ReaderAppBar
 
@@ -70,6 +92,8 @@ fun ReaderSearchScreen(
 
                 }
                 Spacer(modifier = Modifier.height(13.dp))
+                BookList(navController = navController)
+
 
 
             }
@@ -78,6 +102,95 @@ fun ReaderSearchScreen(
 
     }
 
+
+}
+
+@Composable
+fun BookList(
+    navController: NavController,
+    viewModel: ReaderSearchScreenViewModel = hiltViewModel()
+) {
+    val listOfBooks = viewModel.list
+    if (viewModel.isLoading){
+        Row(
+            modifier = Modifier.padding(end = 2.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically) {
+            LinearProgressIndicator()
+            Text(text = "Loading...")
+        }
+
+    }else {
+        LazyColumn(modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp)
+        ){
+            items(items = listOfBooks) { book ->
+                BookRow(book, navController)
+
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun BookRow(
+    book: Item,
+    navController: NavController
+) {
+    Card(modifier = Modifier
+        .clickable {
+            navController.navigate(ReaderScreen.DetailScreen.name + "/${book.id}")
+        }
+        .fillMaxWidth()
+        .height(100.dp)
+        .padding(3.dp),
+        shape = RectangleShape,
+        elevation = CardDefaults.cardElevation(7.dp)) {
+        Row(modifier = Modifier.padding(5.dp),
+            verticalAlignment = Alignment.Top) {
+
+            val imageUrl = if(book.volumeInfo.readingModes.image){
+                book.volumeInfo.imageLinks?.smallThumbnail
+            }
+            else { "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=80&q=80" }
+            Image(
+                painter = rememberAsyncImagePainter(model = imageUrl),
+                contentDescription = "book image",
+                modifier = Modifier
+                    .width(80.dp)
+                    .fillMaxHeight()
+                    .padding(end = 4.dp),
+            )
+
+            Column {
+                Text(text = book.volumeInfo.title, overflow = TextOverflow.Ellipsis)
+                Text(text =  "Author: ${book.volumeInfo.authors}",
+                    overflow = TextOverflow.Clip,
+                    fontStyle = FontStyle.Italic,
+                    style = MaterialTheme.typography.labelSmall)
+
+                Text(text =  "Date: ${book.volumeInfo.publishedDate}",
+                    overflow = TextOverflow.Clip,
+                    fontStyle = FontStyle.Italic,
+                    style = MaterialTheme.typography.labelSmall)
+
+                Text(text =  "${book.volumeInfo.categories}",
+                    overflow = TextOverflow.Clip,
+                    fontStyle = FontStyle.Italic,
+                    style = MaterialTheme.typography.labelSmall)
+
+
+
+
+
+
+            }
+
+        }
+
+    }
 
 }
 
