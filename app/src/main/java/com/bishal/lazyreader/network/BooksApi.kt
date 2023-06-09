a6b412d2747dbdbc1d5cce0dadac5049af95e9c4
Bishal <bishalbera2@gmail.com>
2023-06-09 07:03:56 +0530

    model classes added from the url and booksApi file added

---
+++ app/src/main/java/com/bishal/lazyreader/network/BooksApi.kt
@@ -0,0 +1,20 @@
+package com.bishal.lazyreader.network
+
+import com.bishal.lazyreader.domain.model.Book
+import com.bishal.lazyreader.domain.model.Item
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface BooksApi {
+
+    @GET("volumes")
+    suspend fun getAllBooks(@Query("q") query: String): Book
+
+    @GET("volumes/{bookId}")
+    suspend fun getBookInfo(@Path("bookId") bookId: String): Item
+
+
+}
